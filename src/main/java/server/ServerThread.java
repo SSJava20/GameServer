5098afa0efd1c83c33f58f255c0e7aa6df1284b6
Vladislav <root@stvad.org>
2012-03-18 17:14:03 +0200

    Added mark, state

--- src/main/java/server/ServerThread.java
+++ src/main/java/server/ServerThread.java
@@ -1,10 +1,28 @@
 package server;
 
-public class ServerThread implements Runnable {
+public class ServerThread implements Runnable
+{
+    public char getMark()
+    {
+        return Mark;
+    }
 
-	public void run() {
-		// TODO Auto-generated method stub
+    public void setMark(char mark)
+    {
+        Mark = mark;
+    }
 
-	}
+    char Mark;
+
+    public void run()
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void sendGameState(GameState toSend)
+    {
+
+    }
 
 }
