31d734bc7d448a236ab1982d257bcc6b2194c631
Roman Kostyrko <nubaseg@gmail.com>
2012-03-18 19:59:26 +0300

    servthread

--- src/main/java/server/ServerThread.java
+++ src/main/java/server/ServerThread.java
@@ -1,28 +1,71 @@
 package server;
 
-public class ServerThread implements Runnable
-{
-    public char getMark()
-    {
-        return Mark;
-    }
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.util.Scanner;
 
-    public void setMark(char mark)
-    {
-        Mark = mark;
-    }
+import server.games.Game;
 
-    char Mark;
+public class ServerThread implements Runnable {
 
-    public void run()
-    {
-        // TODO Auto-generated method stub
+	private String name;
+	private Socket clientSocket;
+	private MainServer server;
+	private Game myGame;
+	private Thread playerThread;
+	char Mark;
+	ServerMediator mediator;
 
-    }
+	public ServerThread(ServerMediator mediator) {
+		this.mediator = mediator;
+		this.clientSocket = clientSocket;
+		playerThread = new Thread(this);
+		playerThread.start();
+	}
 
-    public void sendGameState(GameState toSend)
-    {
+	public char getMark() {
+		return Mark;
+	}
 
-    }
+	public void setMark(char mark) {
+		Mark = mark;
+	}
+
+	public void run() {
+		Scanner in = null;
+		try {
+			in = new Scanner(new InputStreamReader(
+					clientSocket.getInputStream()));
+		} catch (IOException e1) {
+			mediator.getServerThreads().remove(this);
+			e1.printStackTrace();
+		}
+		while (true) {
+			String getStringCommand = "";
+			try {
+				getStringCommand = in.nextLine();
+				System.out.println("<INFO> "+getStringCommand);
+				operateCommand(getStringCommand);
+			} catch (Exception e) {
+				try {
+					clientSocket.close();
+					mediator.getServerThreads().remove(this);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				break;
+			}
+		}
+		Thread.currentThread().interrupt();
+	}
+
+	public void sendGameState(GameState toSend) {
+
+	}
+
+	private void operateCommand(String getcommand) {
+
+	}
 
 }
