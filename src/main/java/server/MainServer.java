18d22ca9bc77407cd11fa970d1b2938a62994cc4
Roman Kostyrko <nubaseg@gmail.com>
2012-03-18 16:17:33 +0300

    mainserver

--- src/main/java/server/MainServer.java
+++ src/main/java/server/MainServer.java
@@ -1,13 +1,48 @@
 package server;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
 public class MainServer {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		System.out.print("Test");
+	private InetAddress ipAddress;
+	private int port;
+	private ArrayList<ServerThread> serverThreads;
+	private ServerSocket mServerSocket;
 
+	public MainServer(InetAddress ipAddress, int port) throws IOException {
+		this.ipAddress = ipAddress;
+		this.port = port;
+		serverThreads = new ArrayList<ServerThread>();
+		try {
+			mServerSocket = new ServerSocket(port, 0, ipAddress);
+			System.out.println("<INFO> Server started at "+ipAddress+":"+port);
+			while(true)
+            {
+                Socket nSocket = mServerSocket.accept();
+                System.out.println("<INFO> New client connected");
+                //serverThreads.add(new ServerThread(this, nSocket));
+            }
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			mServerSocket.close();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		try {
+			new MainServer(InetAddress.getByName("127.0.0.1"), 3224);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
